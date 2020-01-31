f75dc21ec347077c95620c483a0bb074ad905050
Chihyu <chihyu10@gmail.com>
2020-01-31 16:35:59 +0100

    Got the code to work somehow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
+
+
 document.getElementById("buttonFood").addEventListener("click",function () {
     var foodInput = document.getElementById("inputFood").value;
 
 
+    //getting images from unsplash
+    let foodImg = fetch(`https://api.unsplash.com/search/photos?page=1&query=${foodInput}&client_id=19637105d5750cc3bb2998bb15bd40378a594c315a2811753cb50b7703c4c094`)
+        .then((unsplashResponse => {
+            return unsplashResponse.json();
+        }))
+        .then(imageData => {
+            foodimageAPI = imageData.results[0].urls.regular;
+            console.log(foodimageAPI)
+        });
 
-
-
-//
     //fetching data from the API
     fetch(`https://api.edamam.com/api/food-database/parser?ingr=${foodInput}&app_id=70227da1&app_key=5d1dbda7ef08ad66aaf00a98e2d219bf`)
 
@@ -13,9 +21,6 @@ document.getElementById("buttonFood").addEventListener("click",function () {
             return response.json();
         }))
         .then(data => {
-
-
-
 
             console.log(data);
             for (let i = 0 ; i < data.hints.length; i++) {
@@ -25,45 +30,28 @@ document.getElementById("buttonFood").addEventListener("click",function () {
                 let foodLabel = data.hints[i].food.label;
                 const upper = foodInput.charAt(0).toUpperCase() + foodInput.substring(1);
 
-
-                    if (ingredCheck === undefined && foodLabel == upper) {
+                    // trying to pick the first one in the array with an ingredient list
+                    if (ingredCheck === undefined && foodLabel == upper || foodLabel == foodInput) {
 
                         console.log("No ingredients!");
 
-                    } else if (ingredCheck != undefined && foodLabel == upper){
+                    } else if (ingredCheck != undefined && foodLabel == upper || foodLabel == foodInput){
 
-                        console.log("elseiffff");
+                        document.getElementById("ing1").innerHTML = ingredCheck;
                         console.log(ingredCheck);
+
+                        //calling the image from unsplash
+                        let cockImg = document.getElementById("cockImg");
+                        cockImg.src =foodimageAPI;
+
+                        //this will stop the loop
                         break;
+
                     }
 
-
-
             }
-            // var foodName = data.hints[0].food.label;
-            // console.log(foodName)
-
-            // if (foodName === upper) {
-            //
-            //     console.log("juiste naam")
-            //
-            // } else {
-            //
-            //     console.log(upper)
-            //     console.log(foodInput);
-            //     console.log("niet juiste@!#!@#!")
-            // }
-            //testing to see if it works
-            // console.log(data);
-            // console.log(data.text);
-            // console.log(data.hints[0].food.label);
-            // console.log(data.hints[0].food.nutrients);
-
-            // let labelName = data.hints[0].food.label;
-            // document.getElementById("mealName").innerHTML = labelName;
 
         });
-
 
 });
 
@@ -71,36 +59,26 @@ document.getElementById("buttonDrink").addEventListener("click",function () {
     let drinks = document.getElementById("inputDrinks").value;
 
 
-    //fetching data from the API
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinks}`)
-    .then((response => {
-        return response.json();
-    }))
-    .then(cocktaildata => {
+        //fetching data from the API
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinks}`)
+        .then((response => {
+            return response.json();
+        }))
+        .then(cocktaildata => {
 
 
-        var cocktail = cocktaildata.drinks[0];
+            var cocktail = cocktaildata.drinks[0];
 
-        document.getElementById("cockName").innerHTML = "Name: " + cocktail.strDrink;
+            document.getElementById("cockName").innerHTML = "Name: " + cocktail.strDrink;
 
-        //getting image of the cocktail and displaying it as an image
-        let cockImg = document.getElementById("cockImg");
-        cockImg.src = cocktail.strDrinkThumb;
+            //getting image of the cocktail and displaying it as an image
+            let cockImg = document.getElementById("cockImg");
+            cockImg.src = cocktail.strDrinkThumb;
 
-        document.getElementById("cockGlass").innerHTML = "Glass: " + cocktail.strGlass;
+            document.getElementById("cockGlass").innerHTML = "Glass: " + cocktail.strGlass;
 
-        document.getElementById("cockInstructions").innerHTML = "Instructions: " + cocktail.strInstructions;
+            document.getElementById("cockInstructions").innerHTML = "Instructions: " + cocktail.strInstructions;
 
-        //testing
-        console.log(cocktail)
-        console.log(cockImg)
-
-        // console.log(cocktaildata);
-        // console.log(cocktaildata.drinks[0].strDrink);
-        // console.log(cocktaildata.drinks[0].strIngredient1);
-        // console.log(cocktaildata.drinks[0].strIngredient2);
-        // console.log(cocktaildata.drinks[0].strGlass);
-        // console.log(cocktaildata.drinks[0].strDrinkThumb);
 
     });
 
